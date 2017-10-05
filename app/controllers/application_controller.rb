016803fd52c3277a276a86ac5547bb2a5274613f
Alexandre Manikowski <alexandre.karpinski.m@gmail.com>
2017-10-05 13:55:21 +0000

    implement basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def hello
-    render html: "oh hi mark"
-  end
+  include SessionsHelper
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -15,17 +15,18 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 
   end
   
-    test "valid signup information" do
+  test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
-      post signup_path, params: { user: { name:  "Example User",
+      post users_path, params: { user: { name:  "Example User",
                                          email: "user@example.com",
                                          password:              "password",
                                          password_confirmation: "password" } }
     end
     follow_redirect!
     assert_template 'users/show'
-    assert_not flash.alert
+    assert is_logged_in?
   end
+  
   
 end
